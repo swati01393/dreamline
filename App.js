0c64c9e234bae94986f2c88127615a0f5c700dde
swati01393 <46031246+swati01393@users.noreply.github.com>
2019-12-12 16:19:37 +0530

    bizeex
    js file

---
+++ App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="App_Aside"> hello world</div>
+      <div classNAme ="App_Form">
+        <div className="PageSwitcher" >
+          <a href="">Sign In</a>
+<a href=""className="PageSwitcher PageSwitcher-active"> Sign Out</a>
+        </div>
+                <div className="FormTitle">
+          <a href="#"className="FormTitle_link">Sign In</a> or <a href="#"className="FormTitle_link">Sign Out</a>
+</div>
+      <div>
+        <label>Name</label>
+        <input type="text"placeholder="enter name here" value="name"></input>  
+         </div>
+         </div> 
+                     
+                
+ 
+         </div>           
+  );
+  }
+export default App;
